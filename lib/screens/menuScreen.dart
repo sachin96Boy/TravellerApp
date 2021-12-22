6c6a1d6f2b0581abfc59047c81634005f6414132
Sachin Supunthaka <sachinsupunthaka96@gmail.com>
2021-12-22 14:15:16 +0530

    fix:add homescreensection and fixing render issues

--- lib/screens/menuScreen.dart
+++ lib/screens/menuScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_traveller_app/widgets/appLargeText.dart';
+import 'package:my_traveller_app/widgets/appText.dart';
 
 class MenuScreen extends StatefulWidget {
   const MenuScreen({Key? key}) : super(key: key);
@@ -14,102 +15,145 @@ class _MenuScreenState extends State<MenuScreen> with TickerProviderStateMixin {
     TabController _tabController = TabController(length: 3, vsync: this);
 
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            padding: const EdgeInsets.only(top: 70, left: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Icon(
-                  Icons.menu,
-                  size: 30,
-                  color: Colors.black,
-                ),
-                Container(
-                  margin: const EdgeInsets.only(right: 20.0),
-                  width: 50.0,
-                  height: 50.0,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: Colors.grey.withOpacity(0.5),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(top: 70, left: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Icon(
+                    Icons.menu,
+                    size: 30,
+                    color: Colors.black,
                   ),
-                  child: const Icon(Icons.person_sharp),
-                )
-              ],
-            ),
-          ),
-          const SizedBox(height: 40),
-          Container(
-            margin: const EdgeInsets.only(left: 20.0),
-            child: const AppLargeText(text: "Discover"),
-          ),
-          const SizedBox(height: 30),
-          Container(
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: TabBar(
-                labelPadding: const EdgeInsets.only(left: 20.0, right: 20.0),
-                controller: _tabController,
-                labelColor: Colors.black,
-                unselectedLabelColor: Colors.grey.withOpacity(0.8),
-                isScrollable: true,
-                indicatorSize: TabBarIndicatorSize.label,
-                indicator: CircleTabIndicator(color: Colors.blue, radius: 4.0),
-                tabs: const [
-                  Tab(
-                    text: "Places",
-                  ),
-                  Tab(
-                    text: "Insperations",
-                  ),
-                  Tab(
-                    text: "Emotions",
-                  ),
+                  Container(
+                    margin: const EdgeInsets.only(right: 20.0),
+                    width: 50.0,
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Colors.grey.withOpacity(0.5),
+                    ),
+                    child: const Icon(Icons.person_sharp),
+                  )
                 ],
               ),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.only(left: 20.0),
-            height: 300.0,
-            width: double.infinity,
-            child: TabBarView(
-              controller: _tabController,
-              children: [
-                ListView.builder(
-                  itemCount: 3,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      margin: const EdgeInsets.only(right: 15.0, top: 10.0),
-                      width: 200.0,
-                      height: 300.0,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20.0),
-                        color: Colors.white,
-                        image: const DecorationImage(
-                            image: NetworkImage(
-                                "https://i.ibb.co/jkM7SL7/wallpaperflare-com-wallpaper.jpg"),
-                            fit: BoxFit.cover),
-                      ),
-                    );
-                  },
-                ),
-                Text("hi"),
-                Text("hi"),
-              ],
+            const SizedBox(height: 25.0),
+            Container(
+              margin: const EdgeInsets.only(left: 20.0),
+              child: const AppLargeText(text: "Discover"),
             ),
-          ),
-          Row(
-            children: const [
-              AppLargeText(
-                text: "Explore More",
-              )
-            ],
-          )
-        ],
+            const SizedBox(height: 18.0),
+            Container(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: TabBar(
+                  labelPadding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                  controller: _tabController,
+                  labelColor: Colors.black,
+                  unselectedLabelColor: Colors.grey.withOpacity(0.8),
+                  isScrollable: true,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  indicator:
+                      CircleTabIndicator(color: Colors.blue, radius: 4.0),
+                  tabs: const [
+                    Tab(
+                      text: "Places",
+                    ),
+                    Tab(
+                      text: "Insperations",
+                    ),
+                    Tab(
+                      text: "Emotions",
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 20.0),
+              height: 300.0,
+              width: double.infinity,
+              child: TabBarView(
+                controller: _tabController,
+                children: [
+                  ListView.builder(
+                    itemCount: 3,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: const EdgeInsets.only(right: 15.0, top: 10.0),
+                        width: 200.0,
+                        height: 300.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.white,
+                          image: const DecorationImage(
+                              image: NetworkImage(
+                                  "https://i.ibb.co/jkM7SL7/wallpaperflare-com-wallpaper.jpg"),
+                              fit: BoxFit.cover),
+                        ),
+                      );
+                    },
+                  ),
+                  Text("hi"),
+                  Text("hi"),
+                ],
+              ),
+            ),
+            const SizedBox(height: 18.0),
+            Container(
+              margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  AppLargeText(
+                    text: "Explore More",
+                    size: 22.0,
+                  ),
+                  AppText(
+                    text: "See more",
+                    color: Colors.black87,
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 9),
+            Container(
+              height: 100.0,
+              width: double.infinity,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 4,
+                  itemBuilder: (_, index) => Container(
+                        margin: const EdgeInsets.only(left: 20.0),
+                        child: Column(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(right: 50.0),
+                              width: 80.0,
+                              height: 80.0,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20.0),
+                                color: Colors.black26,
+                              ),
+                              child: const Icon(Icons.settings),
+                            ),
+                            Container(
+                              child: const AppText(
+                                text: "red",
+                              ),
+                            ),
+                          ],
+                        ),
+                      )),
+            ),
+          ],
+        ),
       ),
     );
   }
